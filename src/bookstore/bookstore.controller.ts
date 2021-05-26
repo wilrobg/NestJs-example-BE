ce91fb3c5236bf8fa7aeb543e90328f5abb8e5ee
Wilfredo Guardado <wilrobgg@gmail.com>
2021-05-25 19:29:23 -0600

    ⚡️ Add cache

--- src/bookstore/bookstore.controller.ts
+++ src/bookstore/bookstore.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, CacheInterceptor, UseInterceptors } from '@nestjs/common';
 import { get } from 'http';
 import { BookstoreService } from './services/bookstore.service';
 import { RequestDto } from "./dto/requestDto.dto";
 
+@UseInterceptors(CacheInterceptor)
 @Controller('bookstore')
 export class BookstoreController {
     constructor(private readonly bookstoreService : BookstoreService){
--- src/bookstore/bookstore.module.ts
+++ src/bookstore/bookstore.module.ts
@@ -1,14 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule, CACHE_MANAGER, Inject, CacheInterceptor } from '@nestjs/common';
 import { BookstoreController } from './bookstore.controller';
 import { BookstoreService } from './services/bookstore.service';
 import { SaleItemRepository } from './repositories/saleitem.repository';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
+      CacheModule.register(),
       TypeOrmModule.forFeature([SaleItemRepository])
     ],
   controllers: [BookstoreController],
-  providers: [BookstoreService]
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    }
+    ,
+    BookstoreService]
 })
-export class BookstoreModule {}
+export class BookstoreModule {
+}
